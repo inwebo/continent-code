6853cc43021df80d45aff77d8a1710f207597373
inwebo <inwebo@gmail.com>
2019-10-03 19:00:01 +0200

    Exception

--- continent-code/Exception/ContinentException.php
+++ continent-code/Exception/ContinentException.php
@@ -1,10 +1,23 @@
 <?php
 
-
 namespace Inwebo\Geo;
 
+use Throwable;
 
-class ContinentException
+class ContinentException extends \Exception
 {
-
+    /**
+     * ContinentException constructor.
+     *
+     * @param string         $message
+     * @param int            $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        if($message === "") {
+            $message = sprintf("Continent code must be a 2 upper letters string");
+        }
+        parent::__construct($message, $code, $previous);
+    }
 }
