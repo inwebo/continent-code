a8dd92f8211e27ab551d8862742e3f82e4aeedca
inwebo <inwebo@gmail.com>
2019-10-03 19:07:40 +0200

    Exception

--- Exception/ContinentException.php
+++ Exception/ContinentException.php
@@ -12,11 +12,12 @@ class ContinentException extends \Exception
      * @param string         $message
      * @param int            $code
      * @param Throwable|null $previous
+     * @param string         $continentCode
      */
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    public function __construct($message = "", $code = 0, Throwable $previous = null, string $continentCode)
     {
         if($message === "") {
-            $message = sprintf("Continent code must be a 2 upper letters string");
+            $message = sprintf("Continent code must be a 2 upper letters string, your input : %s", $continentCode);
         }
         parent::__construct($message, $code, $previous);
     }
